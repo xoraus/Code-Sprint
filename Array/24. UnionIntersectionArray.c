cca08bacf5a732e3029e14739adf1f09fb4f7620
xoraus <imsafay@gmail.com>
2019-02-02 12:26:12 +0530

    sprintv24

---
+++ Array/24. UnionIntersectionArray.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int printUnion(int arr1[], int arr2[], int m, int n) {
+    int i = 0, j = 0;
+    while(i < m && j < n){
+        if(arr1[i] < arr2[j])
+            printf("%d", arr1[i++]);
+        else if (arr2[j] < arr1[i])
+            printf("%d", arr2[j++]);
+        else {
+            printf("%d", arr2[j++]);
+            i++;
+        }
+    }
+
+    // print the remaining elements
+    while(i<m)
+        printf("%d", arr1[i++]);
+    while(j<n)
+        printf("%d", arr2[j++]);
+}
+
+int printIntersection(int arr1[], int arr2[], int m, int n) {
+    int i = 0, j = 0;
+    while(i<m && j<n) {
+        if (arr1[i] < arr2[j])
+            i++;
+        else if (arr2[j] < arr1[i])
+            j++;
+        else if (arr1[i] == arr2[j]) {
+            printf("%d", arr2[j++]);
+            i++;
+        }
+    }
+}
+
+int main() {
+    int arr1[] = {1, 2, 4, 5, 6};
+    int arr2[] = {2, 3, 5, 7};
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+    int n = sizeof(arr2) / sizeof(arr2[0]);
+    printUnion(arr1, arr2, m, n);
+    getchar();
+    return 0;
+}
