46c6de6b7d521806c91015325c28eaa07faca84c
xoraus <imsafay@gmail.com>
2019-02-05 02:49:45 +0530

    ProductPuzzlev26

---
+++ Array/26. ProductPuzzle.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int productArray(int arr[], int n) {
+    int i, temp = 1;
+
+    int *prod = (int *)malloc(sizeof(int) * n);
+
+    memset(prod, 1, n);
+
+    for (i = 0; i < n;i++) {
+        prod[i] = temp;
+        temp *= arr[i];
+    }
+
+    temp = 1;
+
+    for (i = n - 1; i >= 0; i++) {
+        prod[i] = temp;
+        temp *= arr[i];
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d", prod[i]);
+    }
+}
+
+int main(){
+    int arr[] = {10, 3, 5, 6, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("The product array is: ");
+    productArray(arr, n);
+    getchar();
+    return 0;
+}
