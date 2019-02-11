21d36187b4870e1ea383d234cc35b16ab5b70548
xoraus <imsafay@gmail.com>
2019-02-11 20:30:27 +0530

    v11

--- Array/47. FixedPoit.c
+++ Array/47. FixedPoit.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int BinarySearch(int arr[], int low, high) {
+int BinarySearch(int arr[], int low, int high) {
     if(high >= low) {
         int mid = (low + high) / 2;
-        if(mid == arr[i])
+        if(mid == arr[mid])
             return mid;
-        if(mid > arr[i])
+        if(mid > arr[mid])
             return BinarySearch(arr, (mid + 1), high);
-        else {
+        else
             return BinarySearch(arr, low, (mid - 1));
-        }
+
     }
     return -1;
 }
 
 int main() {
-    int arr[] = {-10, -1, ,0 ,3, 10, 11, 30, 50, 100};
+    int arr[] = {-10, -1, 2 ,3, 10, 11, 30, 50, 100};
     int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Fixed point is %d", BinarySearch(arr, 0. n-1));
+    printf("Fixed point is %d", BinarySearch(arr, 0, n-1));
     getchar();
     return 0;
 }
