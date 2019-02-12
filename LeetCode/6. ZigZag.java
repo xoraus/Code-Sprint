acb18c8a739f04dfc410a48cafd9b254e5ba681d
xoraus <imsafay@gmail.com>
2019-02-12 21:04:22 +0530

    codeexpv06

--- LeetCode/6. ZigZag.java
+++ LeetCode/6. ZigZag.java
@@ -15,3 +15,16 @@ public String convert(String s, int nRows) {
         sb[0].append(sb[idx]);
     return sb[0].toString();
 }
+
+/*n=numRows
+Δ=2n-2    1                           2n-1                         4n-3
+Δ=        2                     2n-2  2n                    4n-4   4n-2
+Δ=        3               2n-3        2n+1              4n-5       .
+Δ=        .           .               .               .            .
+Δ=        .       n+2                 .           3n               .
+Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+Δ=2n-2    n                           3n-2                         5n-4
+*/
+//that's the zigzag pattern the question asked!
+//Be careful with nR=1 && nR=2
+
